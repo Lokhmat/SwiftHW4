5aabb7f924761210b3d76af9271fc5977b503b15
lohmat <lokhmatikov.htc@gmail.com>
2021-10-30 19:12:32 +0300

    Project init with one note

---
+++ splokhmatikovPW4/NoteCell.swift
@@ -0,0 +1,14 @@
+//
+//  NoteCell.swift
+//  splokhmatikovPW4
+//
+//  Created by Сергей Лохматиков on 30.10.2021.
+//
+
+import UIKit
+
+class NoteCell: UICollectionViewCell{
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- splokhmatikovPW4/ViewController.swift
+++ splokhmatikovPW4/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var notesCollectionView: UICollectionView!
+    @IBOutlet weak var emptyCollectionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
+                                                "NoteCell", for: indexPath) as! NoteCell
+        cell.titleLabel.text = "Yeah"
+        cell.descriptionLabel.text = "That's greate"
+        return cell
+    }
+    
 }
 
