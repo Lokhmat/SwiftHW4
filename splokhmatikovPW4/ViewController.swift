151d9039e8f3404dd294c0626614dedecd930f59
lohmat <lokhmatikov.htc@gmail.com>
2021-10-30 16:53:09 +0300

    Initial Commit

---
+++ splokhmatikovPW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  splokhmatikovPW4
+//
+//  Created by Сергей Лохматиков on 30.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
