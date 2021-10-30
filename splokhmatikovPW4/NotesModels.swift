6250ad45ba6c1f13677c8d5aed66589985a03815
lohmat <lokhmatikov.htc@gmail.com>
2021-10-30 20:28:01 +0300

    Now you can create notes

---
+++ splokhmatikovPW4/NotesModels.swift
@@ -0,0 +1,13 @@
+//
+//  NotesModels.swift
+//  splokhmatikovPW4
+//
+//  Created by Сергей Лохматиков on 30.10.2021.
+//
+
+import Foundation
+
+struct Note {
+    let title: String
+    let description: String
+}
--- splokhmatikovPW4/ViewController.swift
+++ splokhmatikovPW4/ViewController.swift
@@ -13,24 +13,39 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     @IBOutlet weak var notesCollectionView: UICollectionView!
     @IBOutlet weak var emptyCollectionLabel: UILabel!
+    var notes: [Note] = [] {
+        didSet {
+            emptyCollectionLabel.isHidden = notes.count != 0
+            notesCollectionView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .add, target: self, action:
+                                #selector(createNote(sender:)))
     }
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return notes.count
     }
     
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
-                                                "NoteCell", for: indexPath) as! NoteCell
-        cell.titleLabel.text = "Yeah"
-        cell.descriptionLabel.text = "That's greate"
+                                                        "NoteCell", for: indexPath) as! NoteCell
+        let note = notes[indexPath.row]
+        cell.titleLabel.text = note.title
+        cell.descriptionLabel.text = note.description
         return cell
     }
-    
-}
+    @objc func createNote(sender: UIBarButtonItem) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier:
+                                                                "NoteViewController") as? NoteViewController else { return }
+        vc.outputVC = self
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
+}
